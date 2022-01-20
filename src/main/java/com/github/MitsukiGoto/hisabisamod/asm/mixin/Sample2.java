3445a53c461ef6efd7a0a7c17caa43993f8b8914
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2022-01-21 07:41:15 +0900

    add end_city structure into the plainsBiome

--- src/main/java/com/github/MitsukiGoto/hisabisamod/asm/mixin/Sample2.java
+++ src/main/java/com/github/MitsukiGoto/hisabisamod/asm/mixin/Sample2.java
@@ -1,10 +1,18 @@
 package com.github.MitsukiGoto.hisabisamod.asm.mixin;
 
+import net.minecraft.world.biome.BiomeGenerationSettings;
 import net.minecraft.world.biome.BiomeMaker;
+import net.minecraft.world.gen.feature.structure.StructureFeatures;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BiomeMaker.class)
 public class Sample2 {
-    @ModifyVariable(at=@At("STORE"))
+    @ModifyVariable(method = "plainsBiome(Z)Lnet/minecraft/world/biome/Biome;",at=@At(value="STORE"), ordinal = 0)
+    private static BiomeGenerationSettings.Builder injection(BiomeGenerationSettings.Builder builder) {
+        builder.addStructureStart(StructureFeatures.END_CITY);
+        return builder;
+    }
+
 }
