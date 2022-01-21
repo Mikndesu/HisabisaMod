d4a7799aea74ccab13083804f0af77e5e04c04c8
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2022-01-21 22:44:33 +0900

    Let Creeper spawn where leaves were

--- src/main/java/com/github/MitsukiGoto/hisabisamod/asm/mixin/Sample3.java
+++ src/main/java/com/github/MitsukiGoto/hisabisamod/asm/mixin/Sample3.java
@@ -4,6 +4,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.LanternBlock;
 import net.minecraft.block.LeavesBlock;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.server.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,7 +25,8 @@ public class Sample3 {
     @Inject(at=@At("RETURN"), method= "randomTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/server/ServerWorld;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V")
     private void injection(BlockState p_225542_1_, ServerWorld p_225542_2_, BlockPos p_225542_3_, Random p_225542_4_, CallbackInfo ci){
         if (!p_225542_1_.getValue(PERSISTENT) && p_225542_1_.getValue(DISTANCE) == 7) {
-            p_225542_2_.setBlock(p_225542_3_, Blocks.SEA_LANTERN.defaultBlockState(), 1);
+//            p_225542_2_.setBlock(p_225542_3_, Blocks.SEA_LANTERN.defaultBlockState(), 1);
+            EntityType.CREEPER.spawn(p_225542_2_, null,null, p_225542_3_, SpawnReason.EVENT, true, false);
         }
     }
 }
